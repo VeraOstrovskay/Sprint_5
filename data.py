b792319a23065941ba867e60e3ff0bb99979540d
Vera Ostrovskaya <apz108@mail.ru>
2025-11-13 04:07:35 +0300

    change test_registration and test_login

---
+++ data.py
@@ -0,0 +1,12 @@
+# Данные зарегистрированного аккаунта для проверок с авторизацией
+class Credentials:
+    email ='VeraOstrovskay_33_123@yandex.ru'
+    password ='123456789'
+
+# Текст ссылок в табах конструктора
+class ConstructorTabText:
+    sauces =  'Соусы'
+    rolls = 'Булки'
+    toppings = 'Начинки'
+    
+    
